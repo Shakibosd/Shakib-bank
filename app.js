3472451438329369f16bc2814438971d077854a4
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2023-07-03 04:48:46 +0600

    finsh

--- app.js
+++ app.js
@@ -11,12 +11,51 @@ loginBtn.addEventListener("click", function(){
 
 
 
+
+
+
 /*Deposit Button Event Handler*/
  const depositBtn = document.getElementById("addDeposit");
  depositBtn.addEventListener("click", function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber = parseFloat(depositAmount);
- });
+    const depositNumber = getInputNumber("depositAmount");
+    
+    //Function Call
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    document.getElementById("depositAmount").value  = "";
+ })
+
+
+
+// WithDraw Batton event Handlar
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click",function(){
+   const WithdrawNumber = getInputNumber("withdrawAmount");
+   updateSpanText("currentWithdraw", WithdrawNumber);
+   updateSpanText("currentBalance",-1 * WithdrawNumber);
+
+   document.getElementById("withdrawAmount").value = "";
+})
+
+function getInputNumber(id){
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount)
+    return amountNumber;
+}
+
+
+
+
+
+
+
+ // Function
+function updateSpanText(id, depositNumber){
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount; 
+}
 
 /*Deposit Button Event Handler*/
 
